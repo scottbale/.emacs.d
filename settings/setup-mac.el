1f6ff7525639eefbe3813f586e0e720bab5c39e6
Scott Bale <scott@balehaus.org>
2021-07-02 11:38:50 -0500

    M-backtick bind to ns-next-frame

--- settings/setup-mac.el
+++ settings/setup-mac.el
@@ -4,4 +4,7 @@
 (setq mac-command-modifier 'meta)
 (setq mac-option-modifier nil)
 
+;; Mimic native macOX toggling between windows of same app
+(global-set-key (kbd "M-`") 'ns-next-frame)
+
 (provide 'setup-mac)
