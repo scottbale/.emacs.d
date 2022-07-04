f34b2117212a600a3a152ff83e6b6c4379ac08fb
Scott Bale <scott@balehaus.org>
2022-07-04 15:51:30 -0500

    set projectile project search path

--- settings/setup-projectile.el
+++ settings/setup-projectile.el
@@ -1,5 +1,6 @@
 (projectile-global-mode +1)
 (setq projectile-completion-system 'ivy)
+(setq projectile-project-search-path '("~/.emacs.d/" ("~/dev/nu/" . 1) ("~/dev/" . 1) ("~/dev/clojure/" . 1)))
 ;; (global-set-key (kbd "C-S-n") 'projectile-discover-projects-in-directory)
 
 (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
