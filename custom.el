eae16d79811b247b9b064ad27fb1be2d203e9a1e
Scott Bale <scott@balehaus.org>
2025-05-05 19:23:38 -0500

    custom.el

--- custom.el
+++ custom.el
@@ -4,17 +4,34 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("fee7287586b17efbfda432f05539b58e86e059e78006ce9237b8732fde991b4c" "4c56af497ddf0e30f65a7232a8ee21b3d62a8c332c6b268c81e9ea99b11da0d3" default))
+   '("00445e6f15d31e9afaa23ed0d765850e9cd5e929be5e8e63b114a3346236c44c"
+     "285d1bf306091644fb49993341e0ad8bafe57130d9981b680c1dbd974475c5c7"
+     "57a29645c35ae5ce1660d5987d3da5869b048477a7801ce7ab57bfb25ce12d3e"
+     "fee7287586b17efbfda432f05539b58e86e059e78006ce9237b8732fde991b4c"
+     "4c56af497ddf0e30f65a7232a8ee21b3d62a8c332c6b268c81e9ea99b11da0d3" default))
  '(gnutls-algorithm-priority "normal:-vers-tls1.3")
- ;; for bigger font:
- ;;'(initial-frame-alist '((top . 0) (left . 0) (width . 190) (height . 53)))
- ;; for default case:
- '(initial-frame-alist '((top . 0) (left . 0) (width . 271) (height . 73)))
+ '(ignored-local-variable-values
+   '((projectile-project-test-cmd . "clojure -M:dev:test") (projectile-project-test-cmd . "make")))
+ '(initial-frame-alist '((top . 0) (left . 0) (width . 211) (height . 73)))
  '(package-selected-packages
-   '(lsp-ivy lsp-ui flycheck lsp-treemacs lsp-mode paredit cider clojure-mode exec-path-from-shell solarized-theme dockerfile-mode yaml-mode js2-mode projectile ivy-xref counsel swiper markdown-mode solarized-dark use-package company treemacs dart-mode idle-highlight-mode better-defaults ivy))
+   '(better-defaults cider clojure-mode company counsel dart-mode dockerfile-mode elixir-mode
+                     exec-path-from-shell flycheck go-mode idle-highlight-mode ivy ivy-xref js2-mode
+                     kotlin-mode lsp-ivy lsp-mode lsp-treemacs lsp-ui markdown-mode paredit
+                     plantuml-mode projectile solarized-dark solarized-theme swiper treemacs
+                     use-package yaml-mode))
  '(safe-local-variable-values
-   '((cider-clojure-cli-aliases . ":dev:examples")
-     (cider-clojure-cli-aliases . ":test:dev:local")
+   '((whitespace-line-column . 79)
+     (elisp-lint-indent-specs (if-let* . 2) (when-let* . 1) (let* . defun)
+                              (nrepl-dbind-response . 2) (cider-save-marker . 1)
+                              (cider-propertize-region . 1) (cider-map-repls . 1)
+                              (cider--jack-in . 1) (cider--make-result-overlay . 1)
+                              (insert-label . defun) (insert-align-label . defun)
+                              (insert-rect . defun) (cl-defun . 2) (with-parsed-tramp-file-name . 2)
+                              (thread-first . 0) (thread-last . 0)
+                              (transient-define-prefix . defmacro)
+                              (transient-define-suffix . defmacro))
+     (checkdoc-package-keywords-flag) (cider-clojure-cli-aliases . ":dev")
+     (cider-clojure-cli-aliases . ":dev:examples") (cider-clojure-cli-aliases . ":test:dev:local")
      (cider-clojure-cli-aliases . ":test:examples:dev"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
